93cbb5171354502657ffde8e3c28ba29b6f6aba5
Sean Marks <seanmarks1123@gmail.com>
2025-03-04 20:02:23 -0500

    Errors

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -9,8 +9,16 @@ struct PositiveNonzeroInteger(u64);
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
-        // TODO: This function shouldn't always return an `Ok`.
-        Ok(Self(value as u64))
+        use std::cmp::Ordering;
+        match &value.cmp(&0) {
+           Ordering::Greater => Ok(Self(value as u64)),
+           Ordering::Equal => Err(CreationError::Zero),
+           Ordering::Less => Err(CreationError::Negative),
+        }
+        // if value > 0 {
+        //     Ok(Self(value as u64))
+        // }
+        // else if value == 0 { Err(CreationError::Zero) } else { Err(CreationError::Negative) }
     }
 }
 
